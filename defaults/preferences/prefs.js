683709a9f57714300d1602336389937654aac15f
Infocatcher <infocatcher.dev.null@gmail.com>
2012-12-09 00:31:28 +0400

    Improve preferences description

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,10 +1,10 @@
 pref("extensions.closedownloadtabs.closeSelectedTab", true);
-pref("extensions.closedownloadtabs.maxLoadingWait", 150000); // 2.5*60e3
-pref("extensions.closedownloadtabs.waitDelay", 30);
+pref("extensions.closedownloadtabs.maxLoadingWait", 150000); // 2.5*60e3, don't wait more than this time
+pref("extensions.closedownloadtabs.waitDelay", 30); // Interval to periodically check browser.webProgress.isLoadingDocument
 pref("extensions.closedownloadtabs.waitLoadedTab", 80); // Additional delay for about:neterror?..
 pref("extensions.closedownloadtabs.waitLoadedTab.greasemonkey", 1000); // Additional delay for *.user.js
-pref("extensions.closedownloadtabs.waitDownload", 2000);
-pref("extensions.closedownloadtabs.waitDownloadAction", 1000);
+pref("extensions.closedownloadtabs.waitDownload", 2000); // Delay before tab will be closed (if there is now "locker windows")
+pref("extensions.closedownloadtabs.waitDownloadAction", 1000); // Wait after "locker window" will be closed
 
 // API for another extensions:
 // 1. Add extensions.closedownloadtabs.closeURI.pref.<full_pref_name> = true preference
