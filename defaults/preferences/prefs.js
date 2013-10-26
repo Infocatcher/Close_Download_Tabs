cd3742745faef585042097242591998ff15ca43e
Infocatcher <infocatcher.dev.null@gmail.com>
2013-10-27 00:12:10 +0400

    Correct descriptions

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,14 +1,14 @@
 pref("extensions.closeDownloadTabs.closeSelectedTab", true);
 pref("extensions.closeDownloadTabs.maxLoadingWait", 150000); // 2.5*60e3, don't wait more than this time
 pref("extensions.closeDownloadTabs.waitInterval", 500); // Interval to periodically check browser.webProgress.isLoadingDocument
-// Needs for custombutton://... links (https://addons.mozilla.org/firefox/addon/custom-buttons/)
+// Needs for custombutton://... links (https://addons.mozilla.org/addon/custom-buttons/)
 // and any other custom protocols with nsIProtocolHandler.URI_DOES_NOT_RETURN_DATA flag
 pref("extensions.closeDownloadTabs.waitLoadedTab", 80); // Additional delay for about:neterror?..
 pref("extensions.closeDownloadTabs.waitLoadedTab.greasemonkey", 1000); // Additional delay for *.user.js
 pref("extensions.closeDownloadTabs.waitAfterStopProgress", 10); // Additional delay after STATE_STOP notification
-pref("extensions.closeDownloadTabs.waitDownload", 2000); // Delay before tab will be closed (if there is now "locker windows")
+pref("extensions.closeDownloadTabs.waitDownload", 2000); // Delay before tab will be closed (if there is no "locker windows")
 pref("extensions.closeDownloadTabs.waitDownloadAction", 1000); // Wait after "locker window" will be closed
-pref("extensions.closeDownloadTabs.checkModalInterval", 1500); // We don't remove tab, until opened modal dialog
+pref("extensions.closeDownloadTabs.checkModalInterval", 1500); // We don't remove tab until opened modal dialog
 // See https://github.com/Infocatcher/Close_Download_Tabs/issues/1 for details
 
 // API for another extensions:
