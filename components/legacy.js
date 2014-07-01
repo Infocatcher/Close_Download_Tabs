c4b925b4ab9b546d9caff0b531077dc7f6668e86
Infocatcher <infocatcher.dev.null@gmail.com>
2014-07-01 15:22:09 +0400

    Reformat polyfill for Function.prototype.bind() + use Array.slice() instead of Array.prototype.slice.call()

--- components/legacy.js
+++ components/legacy.js
@@ -46,29 +46,28 @@ const Services = {
 	}
 };
 
-// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-
-    var aArgs = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP
-                                 ? this
-                                 : oThis,
-                               aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-  };
+// Based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+if(!Function.prototype.bind) {
+	Function.prototype.bind = function(oThis) {
+		if(typeof this !== "function") {
+			// closest thing possible to the ECMAScript 5 internal IsCallable function
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		}
+		var aArgs = Array.slice(arguments, 1),
+			fToBind = this,
+			fNOP = function() {},
+			fBound = function() {
+				return fToBind.apply(
+					this instanceof fNOP && oThis
+						? this
+						: oThis,
+					aArgs.concat(Array.slice(arguments))
+				);
+			};
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+		return fBound;
+	};
 }
 
 // https://developer.mozilla.org/en/Extensions/Bootstrapped_extensions#Reason_constants
