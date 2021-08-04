8a862aad313f3cf6a85659002d28b1569be30593
Infocatcher <infocatcher.dev.null@gmail.com>
2021-08-04 21:56:51 +0300

    Improve startup performance: use lazy loading for prefs

---
+++ prefs.js
@@ -0,0 +1,93 @@
+var prefs = {
+	ns: "extensions.closeDownloadTabs.",
+	initialized: false,
+	init: function() {
+		if(this.initialized)
+			return;
+		this.initialized = true;
+
+		//~ todo: add new condition when https://bugzilla.mozilla.org/show_bug.cgi?id=564675 will be fixed
+		if(platformVersion >= 2)
+			this.loadDefaultPrefs();
+		Services.prefs.addObserver(this.ns, this, false);
+	},
+	destroy: function() {
+		if(!this.initialized)
+			return;
+		this.initialized = false;
+
+		Services.prefs.removeObserver(this.ns, this);
+	},
+
+	observe: function(subject, topic, pName) {
+		if(topic != "nsPref:changed")
+			return;
+		var pVal = this.getPref(pName);
+		var shortName = pName.substr(this.ns.length);
+		this._cache[shortName] = pVal;
+	},
+
+	loadDefaultPrefs: function() {
+		this._cache = { __proto__: null }; // prefs.get() will save wrong value in cache, if called before prefs.init()
+		var defaultBranch = Services.prefs.getDefaultBranch("");
+		var prefsFile = rootURI + "defaults/preferences/prefs.js";
+		var prefs = this;
+		Services.scriptloader.loadSubScript(prefsFile, {
+			pref: function(pName, val) {
+				var pType = defaultBranch.getPrefType(pName);
+				if(pType != defaultBranch.PREF_INVALID && pType != prefs.getValueType(val)) {
+					Components.utils.reportError(
+						LOG_PREFIX + 'Changed preference type for "' + pName
+						+ '", old value will be lost!'
+					);
+					defaultBranch.deleteBranch(pName);
+				}
+				prefs.setPref(pName, val, defaultBranch);
+			}
+		});
+	},
+
+	_cache: { __proto__: null },
+	get: function(pName, defaultVal) {
+		var cache = this._cache;
+		return pName in cache
+			? cache[pName]
+			: (cache[pName] = this.getPref(this.ns + pName, defaultVal));
+	},
+	set: function(pName, val) {
+		return this.setPref(this.ns + pName, val);
+	},
+	getPref: function(pName, defaultVal, prefBranch) {
+		var ps = prefBranch || Services.prefs;
+		switch(ps.getPrefType(pName)) {
+			case ps.PREF_BOOL:   return ps.getBoolPref(pName);
+			case ps.PREF_INT:    return ps.getIntPref(pName);
+			case ps.PREF_STRING: return ps.getComplexValue(pName, Components.interfaces.nsISupportsString).data;
+		}
+		return defaultVal;
+	},
+	setPref: function(pName, val, prefBranch) {
+		var ps = prefBranch || Services.prefs;
+		var pType = ps.getPrefType(pName);
+		if(pType == ps.PREF_INVALID)
+			pType = this.getValueType(val);
+		switch(pType) {
+			case ps.PREF_BOOL:   ps.setBoolPref(pName, val); break;
+			case ps.PREF_INT:    ps.setIntPref(pName, val);  break;
+			case ps.PREF_STRING:
+				var ss = Components.interfaces.nsISupportsString;
+				var str = Components.classes["@mozilla.org/supports-string;1"]
+					.createInstance(ss);
+				str.data = val;
+				ps.setComplexValue(pName, ss, str);
+		}
+		return this;
+	},
+	getValueType: function(val) {
+		switch(typeof val) {
+			case "boolean": return Services.prefs.PREF_BOOL;
+			case "number":  return Services.prefs.PREF_INT;
+		}
+		return Services.prefs.PREF_STRING;
+	}
+};
